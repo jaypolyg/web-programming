f0f09aa1b107ed885bbb195ade7836ccf0d05f83
jaypolyg <ipolygenis@athtech.gr>
2026-01-23 22:46:12 +0200

    Delete kanaris-cooks directory

--- kanaris-cooks/js/auth.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll("form").forEach(form=>{
-  form.addEventListener("submit", e=>{
-    e.preventDefault();
-    window.location.href="home.html";
-  });
-  localStorage.setItem("username", usernameInput.value);
-});
